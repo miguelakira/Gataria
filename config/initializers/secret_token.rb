6d6976b4f55e7b02e1dde7d4f15d1924580767f3
Miguel Akira <miguelakira@Akira.local>
2012-02-22 12:26:44 -0200

    commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gataria::Application.config.secret_token = '6bda76ab7fc7a74cc98221b1f0508766842b72d62cea11bc306029fabc51c0f4bfa56a5eebe1c8de815d5352b0688f392e5669a6c987106c61cdcafc6cc8499b'
